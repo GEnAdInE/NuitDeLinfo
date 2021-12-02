cbba674d2b027d24675c204328f4dcdb98cb14ed
GEnAdInE <one.robin@outlook.fr>
2021-12-02 22:33:29 +0100

    Add firebase + basic login

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
+import * as firebase from 'firebase/app';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAUgICpXRIYAmDY43GcRl2e0NoPkAqnK5Y',
+  authDomain: 'snsm-e12d3.firebaseapp.com',
+  projectId: 'snsm-e12d3',
+  storageBucket: 'snsm-e12d3.appspot.com',
+  messagingSenderId: '1098742023798',
+  appId: '1:1098742023798:web:d78b2abd17af9a63f74157',
+};
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(store).use(router).mount('#app');
