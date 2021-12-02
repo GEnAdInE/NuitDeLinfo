1af0d2f0c9ef8f8b064f98ae09b20ebbac5db3bc
skatos-ux <nicolas.magne1@outlook.fr>
2021-12-02 21:40:48 +0100

    Init

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,26 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+$color_main: #5099EE;
 * {
   margin: 0;
+  border: none;
+  font-family: Montserrat, sans-serif;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 .page {
   width: 100vw;
   height: 100vh;
+}
+.button__primary {
+  border-radius: 15px;
+  background-color: $color_main;
+  padding: 0.8rem;
+  border: 2px solid darken($color_main, 5);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="page home">
-    Hello
+    <Navbar></Navbar>
+    <div class="home__search">
+      <input type="text" placeholder="Martin Matin">
+    </div>
+    <section>
+      Lorem Ipsum
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Navbar from '@/components/Navbar.vue';
 
 @Options({
   components: {
+    Navbar,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+}
 </script>
