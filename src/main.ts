67dadc1ec167428db9b449e0de73e872fba155b1
GEnAdInE <one.robin@outlook.fr>
2021-12-03 02:31:48 +0100

    fixing firebasebug

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import * as firebase from 'firebase/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faShip } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -10,16 +9,6 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAUgICpXRIYAmDY43GcRl2e0NoPkAqnK5Y',
-  authDomain: 'snsm-e12d3.firebaseapp.com',
-  projectId: 'snsm-e12d3',
-  storageBucket: 'snsm-e12d3.appspot.com',
-  messagingSenderId: '1098742023798',
-  appId: '1:1098742023798:web:d78b2abd17af9a63f74157',
-};
-firebase.initializeApp(firebaseConfig);
 
 library.add(faShip);
 
--- src/utils/FirebaseInterface.ts
+++ src/utils/FirebaseInterface.ts
@@ -1,8 +1,7 @@
 import {
   doc, query, collection, getDocs, getFirestore, getDoc, updateDoc, deleteDoc, addDoc,
 } from 'firebase/firestore';
-import firebase from 'firebase/compat';
-import Timestamp = firebase.firestore.Timestamp;
+import * as firebase from 'firebase/app';
 
 interface Article{
   Description: string,
@@ -16,10 +15,22 @@ interface Pending{
   Titre: string,
   ID: string,
   IsNewArticle: boolean,
-  Date: Timestamp,
+  Date: Date,
 }
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyAUgICpXRIYAmDY43GcRl2e0NoPkAqnK5Y',
+  authDomain: 'snsm-e12d3.firebaseapp.com',
+  projectId: 'snsm-e12d3',
+  storageBucket: 'snsm-e12d3.appspot.com',
+  messagingSenderId: '1098742023798',
+  appId: '1:1098742023798:web:d78b2abd17af9a63f74157',
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const db = getFirestore();
+
 async function getAllPending() {
   const q = query(collection(db, 'Pending'));
   const querySnapshot = await getDocs(q);
