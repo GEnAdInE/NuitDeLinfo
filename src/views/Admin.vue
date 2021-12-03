7238fa454de75df1c138f935faeb28a97b95bd9f
baptouley <baptiste.lerouley1@gmail.com>
2021-12-03 05:45:39 +0100

    commit admin table

---
+++ src/views/Admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page admin">
+    <TableAdmin></TableAdmin>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import TableAdmin from '@/components/TableAdmin.vue';
+
+@Options({
+  components: {
+    TableAdmin,
+  },
+})
+export default class Admin extends Vue {}
+</script>
+
+<style scoped>
+
+</style>
